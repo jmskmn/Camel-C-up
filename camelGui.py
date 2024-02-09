4479f09d7ed8a5fb73ba858ca6cf60e86f6c1461
Paul Ophoff <ophoff.paul@gmail.com>
2024-02-09 10:53:17 -0600

    Added exit button to camelGui.py

--- camelGui.py
+++ camelGui.py
@@ -4,10 +4,19 @@
 import tkinter
 from tkinter import messagebox
 
+def exit():
+    warning = tkinter.messagebox.askyesno("Warning!", "are you sure you want to exit?")
+    if warning:
+        main_window.destroy()
+
 main_window = tkinter.Tk()
 main_window.geometry('1000x500')
 main_window.title("Camel Up Board Game")
 
 Board = tkinter.Frame(main_window)
 Board.grid()
+
+exit_button = tkinter.Button(Board, text="Exit", bg=("red"), command=exit)
+exit_button.grid(row=0, column=0)
+
 Board.mainloop()
