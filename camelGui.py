5e0367a3accfaec34d941b18c9b9a1c6b4eb5df5
Paul Ophoff <ophoff.paul@gmail.com>
2024-02-08 21:15:47 -0600

    Added frame for GUI

---
+++ camelGui.py
@@ -0,0 +1,13 @@
+# GUI for Camel-C-Up
+# Paul and Wyatt
+
+import tkinter
+from tkinter import messagebox
+
+main_window = tkinter.Tk()
+main_window.geometry('1000x500')
+main_window.title("Camel Up Board Game")
+
+Board = tkinter.Frame(main_window)
+Board.grid()
+Board.mainloop()
